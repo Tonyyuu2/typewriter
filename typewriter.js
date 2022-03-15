e54f19fc18519d55fd9202876f3e82086179c898
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-03-15 23:43:14 +0000

    types message with typewriter animation

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+let sentence = "hello there from lighthouse labs";
+
+sentence += `\n`
+
+let delay = 0
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay += 50)
+}
